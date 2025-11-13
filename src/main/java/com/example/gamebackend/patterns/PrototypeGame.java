710cc29a039aba966168fca53f7bfc9fdcfdc4cd
BurbanoValentina <valentina.burbanos@campusucc.edu.co>
2025-11-13 12:29:50 -0500

    Add PrototypeGame

---
+++ src/main/java/com/example/gamebackend/patterns/PrototypeGame.java
@@ -0,0 +1,23 @@
+package com.example.gamebackend.patterns;
+
+import com.example.gamebackend.model.Game;
+
+public class PrototypeGame implements Cloneable {
+    private final Game game;
+
+    public PrototypeGame(Game game) {
+        this.game = game;
+    }
+
+    public Game cloneGame() {
+        Game clone = new Game();
+        clone.setName(game.getName());
+        clone.setGenre(game.getGenre());
+        clone.setScore(game.getScore());
+        clone.setCorrectAnswers(game.getCorrectAnswers());
+        clone.setTotalQuestions(game.getTotalQuestions());
+        clone.setDurationSeconds(game.getDurationSeconds());
+        clone.setCreatedAt(game.getCreatedAt());
+        return clone;
+    }
+}
